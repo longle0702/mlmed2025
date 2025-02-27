186cf823a154aec566f64edfadef27075032ba99
LongLe <lelinhlong@gmail.com>
2025-02-27 14:04:14 +0700

    Updated Labwork 1

--- Labwork 1/p_prepare_data.py
+++ Labwork 1/p_prepare_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
 
 class P_Data:
     pa = pd.read_csv(r'D:\USTH\\Nam Ba\\ml in med\\mlmed2025\\Labwork 1\\data\\ptbdb_abnormal.csv', header=None)
@@ -15,10 +14,6 @@ class P_Data:
     p_data = p.iloc[:, :-1]
 
     p_train, p_test, p_train_label, p_test_label = train_test_split(p_data, p_label, test_size=0.2, random_state=42)
-
-    smote = SMOTE(random_state=42)
-    p_train, p_train_label = smote.fit_resample(p_train, p_train_label)
-
     p_train_label = p_train_label.map(classes)
     p_test_label = p_test_label.map(classes)
 
